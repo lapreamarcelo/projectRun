e32f59668f7808ca5160fc9d5f0262748e217048
Marcelo Laprea <marcelojosel15@gmail.com>
2021-11-05 16:53:20 +0100

    First commit

---
+++ Intervals running/Intervals running/Intervals_runningApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intervals_runningApp.swift
+//  Intervals running
+//
+//  Created by Marcelo Laprea on 5/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Intervals_runningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
