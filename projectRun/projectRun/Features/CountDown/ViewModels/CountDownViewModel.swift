4fed99b8da62c0a8227df85c1eec2db374c057ff
sebasf8 <sebasf8@gmail.com>
2021-11-06 01:51:46 +0100

    running publisher

--- projectRun/projectRun/Features/CountDown/ViewModels/CountDownViewModel.swift
+++ projectRun/projectRun/Features/CountDown/ViewModels/CountDownViewModel.swift
@@ -11,6 +11,8 @@ import Combine
 
 class CountDownViewModel: ObservableObject {
     @Published var currentNumber: String
+    @Published var running: Bool
+
     private var startedAt: Date
 
     private let countdown: Countdown
@@ -19,16 +21,19 @@ class CountDownViewModel: ObservableObject {
     init(seconds: Int) {
         startedAt = Date()
         currentNumber = String(seconds)
+        running = false
 
         countdown = Countdown(seconds: seconds)
     }
 
     func startCountdown() {
+        running = true
         timerSubscription = countdown.start()
             .sink { [weak self] currentNumber in
                 guard currentNumber > 0 else {
                     self?.timerSubscription?.cancel()
                     self?.currentNumber = String("0")
+                    self?.running = false
                     return
                 }
 
