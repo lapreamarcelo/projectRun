bb32151ef5988174c9a6de2c70be81238c896899
Libranner <libranner@gmail.com>
2021-11-06 13:41:42 +0100

    Refactor views

---
+++ projectRun/projectRun/Features/Profile/Views/ProfileView.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileView.swift
+//  projectRun
+//
+//  Created by Libranner Leonel Santos Espinal on 6/11/21.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+      Form {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Petizo")
+      }
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
