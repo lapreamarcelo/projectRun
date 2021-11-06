4c168e7e57422a01b5be0bc25a9048efc64f9e76
Marcelo Laprea <marcelojosel15@gmail.com>
2021-11-06 12:01:02 +0100

    Create workout start point in AppleWatch

---
+++ projectRun/projectRunWatch WatchKit Extension/WorkoutView.swift
@@ -0,0 +1,25 @@
+//
+//  WorkoutView.swift
+//  projectRunWatch WatchKit Extension
+//
+//  Created by Marcelo Laprea on 6/11/21.
+//
+
+import SwiftUI
+
+struct WorkoutView: View {
+    @ObservedObject var workoutManager = WorkoutManager()
+    
+    var body: some View {
+        MetricsView(workoutManager: workoutManager)
+            .onAppear {
+                workoutManager.authorizeHealthKit()
+            }
+    }
+}
+
+struct WorkoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        WorkoutView()
+    }
+}
